d260d615942317c5dfb4f7a8c125396527616620
Marco Beraldi <marcobr92@hotmail.com>
2020-12-22 16:39:14 -0300

    Update maior_ou_menor.rb
    inclusão de loop para novas tentativas

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -5,25 +5,34 @@ puts "Hello World"
 puts "Please, whats your name?"
 name = gets
 puts "Hi, " + name
-puts "You are playing a guessing number game, please inform a number between 1 and 10 and see if you guessed correctly."
+puts "You are playing a guessing number game, please inform a number between 1 and 10 and see if you guessed it correctly."
+puts "Remember, you can guess the number 3 times"
 secret = 8
-guessed = gets
-puts "You guessed " + guessed
-puts "Is it the correct number?"
-puts "..."
 
-correct = guessed.to_i == secret
-if correct
-	puts "You guessed the correct number " + name
-	puts "Congrats!"
-else
-	puts "You didn't guessed the correct number " + name
-	puts "Please try again."
-	bigger = guessed.to_i > secret
-	if bigger
-		puts "Your number is bigger than the secret number"
+
+num_of_tries = 3
+for tries in 1..num_of_tries
+	puts "Try " + tries.to_s + " of " + num_of_tries.to_s
+
+	guessed = gets
+	puts "You guessed " + guessed
+	puts "Is it the correct number?"
+	puts "..."
+
+	correct = guessed.to_i == secret
+	if correct
+		puts "You guessed the correct number " + name
+		puts "Congrats!"
+		break
 	else
-		puts "The secret number is bigger than the guessed number"
+		puts "You didn't guessed the correct number " + name
+		puts "Please try again."
+		bigger = guessed.to_i > secret
+		if bigger
+			puts "Your number is bigger than the secret number"
+		else
+			puts "The secret number is bigger than the guessed number"
+		end
 	end
 end
 
