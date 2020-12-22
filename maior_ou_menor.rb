8febf7628402ead4a51b2049348612ae65bafceb
Marco Beraldi <marcobr92@hotmail.com>
2020-12-22 14:54:00 -0300

    Update maior_ou_menor.rb
    inclusão de logica simples + conversão p/ eng.

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,4 +1,15 @@
-puts "Ola mundo"
-puts "Qual o seu nome?"
-nome = gets
-puts "Ola, " + nome
+#Marco Beraldi
+#Treinamento Ruby Alura
+#22/12/2020
+puts "Hello World"
+puts "Please, whats your name?"
+name = gets
+puts "Hi, " + name
+puts "You are playing a guessing number game, please inform a number between 1 and 10 and see if you guessed correctly."
+secret = 8
+guessed = gets
+puts "You guessed " + guessed
+puts "Is it the correct number?"
+puts "Response: "
+puts guessed.to_i == secret
+puts "Tks for playing " + name
